49417e6093630640f8c774fefc835932b545c588
Auston Stewart <auston@shedbuilt.net>
2018-01-09 19:32:45 -0500

    Updating to e2fsprogs 1.43.8

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 mkdir -v build
 cd build
+if [ "$SHED_BUILDMODE" == 'bootstrap' ]; then
 LIBS=-L/tools/lib                    \
 CFLAGS=-I/tools/include              \
 PKG_CONFIG_PATH=/tools/lib/pkgconfig \
@@ -11,10 +12,20 @@ PKG_CONFIG_PATH=/tools/lib/pkgconfig \
              --disable-libblkid      \
              --disable-libuuid       \
              --disable-uuidd         \
-             --disable-fsck
-make -j $SHED_NUMJOBS
-make DESTDIR=${SHED_FAKEROOT} install
-make DESTDIR=${SHED_FAKEROOT} install-libs
+             --disable-fsck || exit 1
+else
+../configure --prefix=/usr           \
+             --bindir=/bin           \
+             --with-root-prefix=""   \
+             --enable-elf-shlibs     \
+             --disable-libblkid      \
+             --disable-libuuid       \
+             --disable-uuidd         \
+             --disable-fsck || exit 1
+fi
+make -j $SHED_NUMJOBS || exit 1
+make DESTDIR=${SHED_FAKEROOT} install || exit 1
+make DESTDIR=${SHED_FAKEROOT} install-libs || exit 1
 # Make these writable so they can be stripped later
 chmod -v u+w ${SHED_FAKEROOT}/usr/lib/{libcom_err,libe2p,libext2fs,libss}.a
 gunzip -v ${SHED_FAKEROOT}/usr/share/info/libext2fs.info.gz
