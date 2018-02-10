95776752657107eb5d899cccc7cfb152ebdae5fd
Auston Stewart <auston@shedbuilt.net>
2018-02-10 07:07:20 -1000

    Updating to 1.43.9

--- build.sh
+++ build.sh
@@ -24,10 +24,10 @@ else
              --disable-fsck || exit 1
 fi
 make -j $SHED_NUMJOBS || exit 1
-make DESTDIR=${SHED_FAKEROOT} install || exit 1
-make DESTDIR=${SHED_FAKEROOT} install-libs || exit 1
+make DESTDIR="$SHED_FAKEROOT" install || exit 1
+make DESTDIR="$SHED_FAKEROOT" install-libs || exit 1
 # Make these writable so they can be stripped later
-chmod -v u+w ${SHED_FAKEROOT}/usr/lib/{libcom_err,libe2p,libext2fs,libss}.a
-gunzip -v ${SHED_FAKEROOT}/usr/share/info/libext2fs.info.gz
+chmod -v u+w "${SHED_FAKEROOT}"/usr/lib/{libcom_err,libe2p,libext2fs,libss}.a
+gunzip -v "${SHED_FAKEROOT}/usr/share/info/libext2fs.info.gz"
 makeinfo -o doc/com_err.info ../lib/et/com_err.texinfo
-install -v -m644 doc/com_err.info ${SHED_FAKEROOT}/usr/share/info
+install -v -m644 doc/com_err.info "${SHED_FAKEROOT}/usr/share/info"
